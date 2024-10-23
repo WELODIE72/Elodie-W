1aa584e4e7c134be79c12974702f50cf6b5996ff
Excellencea236 <excellenceacademia236@gmail.com>
2024-10-23 15:25:10 -0700

    Mise à jour du dépôt avec mes modifications

--- 2_docker_sql/pipeline.py
+++ 2_docker_sql/pipeline.py
@@ -1,3 +1,11 @@
+# Script Python destiné à afficher des arguments de ligne de commande et formater une chaîne
+
+import sys
 import pandas as pd
 
-print('job finished successfully')
+print(sys.argv)
+
+day = sys.argv[1]
+
+# formatage de la chaîne pour construire des chaînes dynamiques
+print(f'job finished successfully for day = {day}')
